5afd6da6065fd2edc57a46cdaca9e4f681f52bc8
村山 <miramura0901@gmail.com>
2017-12-20 10:49:26 +0900

    PlayerStatusの仮完成

--- Team27_RougeLike/Team27_RougeLike/Team27_RougeLike/Object/PlayerStatus.cs
+++ Team27_RougeLike/Team27_RougeLike/Team27_RougeLike/Object/PlayerStatus.cs
@@ -9,26 +9,52 @@ namespace Team27_RougeLike.Object
     class PlayerStatus
     {
         private Status status;
+        private Status baseStatus;
         private Inventory inventory;
 
         private int addPower;
         private int addDefence;
-        private float wight;
+        private float weight;
 
         public PlayerStatus(Status status)
         {
-            this.status = status;
+            this.baseStatus = status;
             inventory = new Inventory();
         }
 
         public void Initialize()
         {
-            
+            status = new Status(baseStatus.Level, baseStatus.Health, baseStatus.BasePower, baseStatus.BaseArmor, baseStatus.Attackspd, baseStatus.Movespeed);
         }
 
         public void AddStatus()
         {
+            inventory.GetStatus(ref addPower, ref addDefence, ref weight);
+        }
 
+        public int GetPower()
+        {
+            return status.BasePower + addPower;
+        }
+
+        public int GetDefence()
+        {
+            return status.BaseArmor + addDefence;
+        }
+
+        public int GetAttackSpeed()
+        {
+            return status.Attackspd + (int)(weight / 10 / status.BasePower);
+        }
+
+        public float GetVelocty()
+        {
+            return status.Movespeed - weight / 100 / status.BasePower;
+        }
+
+        public Inventory GetInventory()
+        {
+            return inventory;
         }
     }
 }
