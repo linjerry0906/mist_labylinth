037b5d1324131945b6c679b0709c96b6534b8fb7
リン <linjerry0906@gmail.com>
2017-12-20 14:16:46 +0900

    PauseUI追加

---
+++ Team27_RougeLike/Team27_RougeLike/Team27_RougeLike/UI/PauseUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Team27_RougeLike.Device;
+using Team27_RougeLike.Scene;
+
+namespace Team27_RougeLike.UI
+{
+    class PauseUI
+    {
+        private GameDevice gameDevice;
+        private GameManager gameManager;
+        private InputState input;
+        private Renderer renderer;
+
+        private Window backLayer;
+
+        public PauseUI(GameManager gameManager, GameDevice gameDevice)
+        {
+            this.gameDevice = gameDevice;
+            this.gameManager = gameManager;
+            input = gameDevice.InputState;
+            renderer = gameDevice.Renderer;
+
+
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+        }
+    }
+}
