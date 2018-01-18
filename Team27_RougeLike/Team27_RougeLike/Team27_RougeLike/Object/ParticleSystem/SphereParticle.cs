89ae95d1b60fbc6be2c97619e78debb43db09199
リン <linjerry0906@gmail.com>
2018-01-18 16:36:38 +0900

    作業保存

---
+++ Team27_RougeLike/Team27_RougeLike/Team27_RougeLike/Object/ParticleSystem/SphereParticle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Team27_RougeLike.Device;
+
+namespace Team27_RougeLike.Object.ParticleSystem
+{
+    class SphereParticle : Particle
+    {
+        public SphereParticle(GameDevice gameDevice)
+            :base(gameDevice)
+        {
+        }
+
+        public override void Draw(Renderer renderer)
+        {
+        }
+
+        public override void Initialize()
+        {
+            isDead = false;
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+    }
+}
