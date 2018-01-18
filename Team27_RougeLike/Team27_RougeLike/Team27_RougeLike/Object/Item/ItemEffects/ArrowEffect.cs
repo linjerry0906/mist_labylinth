291c548da4f88d892b56a7c1d12d9df480d0efa4
村山 <miramura0901@gmail.com>
2018-01-18 22:06:00 +0900

    ItemEffectにArrowEffectを追加 ConsumptionItemにスタック数を追加

---
+++ Team27_RougeLike/Team27_RougeLike/Team27_RougeLike/Object/Item/ItemEffects/ArrowEffect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team27_RougeLike.Object.Item.ItemEffects
+{
+    class ArrowEffect : ItemEffect
+    {
+        private int power;
+
+        public ArrowEffect(int power)
+        {
+            this.power = power;
+        }
+
+        public int GetPower()
+        {
+            return power;
+        }
+    }
+}
